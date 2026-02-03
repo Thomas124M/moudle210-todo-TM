32677734111d914fdc4a6884759cbadaf97ef173
ThomasMao <thomas.mao@studentfr.ch>
2026-02-03 08:44:24 +0100

    modification URL

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://container-mao.salmonpebble-2794c555.northeurope.azurecontainerapps.io";
+const apiEndpoint = "https://container-mao.salmonpebble-2794c555.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
