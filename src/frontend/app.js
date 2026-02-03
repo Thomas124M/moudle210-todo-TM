3e339e89e65b804c9ed90fd67800a804b360431e
ThomasMao <thomas.mao@studentfr.ch>
2026-02-03 08:28:03 +0100

    test modification

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://container-mao.salmonpebble-2794c555.northeurope.azurecontainerapps.io";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
